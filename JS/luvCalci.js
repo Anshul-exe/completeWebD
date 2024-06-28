51db95b63c46b2902110bf89854e49635f9a3a48
anshul-exe <anshulchauhan1224@gmail.com>
2024-06-29 00:53:21 +0530

    twelfth day

--- JS/luvCalci.js
+++ JS/luvCalci.js
@@ -1,4 +1,4 @@
 prompt("What's your name??")
 prompt("What is your crush's name??")
-var l = Math.floor(Math.random() * 100) + 1
-alert("you both are "+l+"% compatible")
+var luv = Math.floor(Math.random() * 100) + 1
+alert("you both are "+luv+"% compatible")
