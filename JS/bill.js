7f307abc96e27a8d9601a07bd300f25f497a11f0
Anshul-exe <anshulchauhanworks@gmail.com>
2024-07-10 06:20:24 +0530

    bhool gae ab toh

--- JS/bill.js
+++ JS/bill.js
@@ -1,7 +1,7 @@
 function whosPaying(names) {
     
     const nameList = names.length;
-    const nameChoice = Math.floor(Math.random()* nameList);
+    const nameChoice = Math.floor(Math.random()*nameList);
     const personSelected = names[nameChoice];
     
     return personSelected + " is going to buy lunch today";
