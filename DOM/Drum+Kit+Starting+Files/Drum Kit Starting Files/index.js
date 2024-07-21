388f3e31b9ad3b2d4a38ab2c2229f3312ff06493
Anshul-exe <anshulchauhanworks@gmail.com>
2024-07-21 06:44:51 +0530

    Dice game

---
+++ DOM/Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
@@ -0,0 +1,56 @@
+// Detecting Click press for sound
+var DrumNum = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < DrumNum; i++) {
+
+document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+    sound(this.innerHTML)
+} )}
+//___________________________________________________________
+
+// Detecting Keyboard press for sound
+document.addEventListener("keydown", function (event) {
+    sound(event.key)
+});
+//___________________________________________________________
+
+// Phle part me innerHTML ko sound function me pass kia as a char, aur fir keypress se sound function me char pas kiya
+// phir sound function me switch case se sound ko play kia
+
+// Sound function
+function sound(key){        
+    switch (key) {
+        case "a":
+            var kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+        case "s":
+            var snare = new Audio ('./sounds/snare.mp3');
+            snare.play()
+            break;
+        case "d":
+            var tom3 = new Audio ('./sounds/tom-3.mp3');
+            tom3.play()
+            break;
+        case "f":
+            var tom1 = new Audio ('./sounds/tom-1.mp3');
+            tom1.play()
+            break;
+        case "j":
+            var tom2 = new Audio ('./sounds/tom-2.mp3');
+            tom2.play()
+            break;
+        case "k":
+            var crash = new Audio ('./sounds/crash.mp3');
+            crash.play()
+            break;
+        case "l":
+            var tom4 = new Audio ('./sounds/tom-4.mp3');
+            tom4.play()
+            break;
+    
+        default:
+            console.log(this.innerHTML)
+            break;
+    }
+}
