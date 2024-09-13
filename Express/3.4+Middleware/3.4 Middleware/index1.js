91807ade55fc717898c9e709d96c92cf2c9787ea
Anshul-exe <anshulchauhanworks@gmail.com>
2024-09-14 05:03:33 +0530

    middleware

--- Express/3.4+Middleware/3.4 Middleware/index1.js
+++ Express/3.4+Middleware/3.4 Middleware/index1.js
@@ -1,4 +1,5 @@
 import express from "express";
+import bodyParser from "body-parser";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
@@ -7,7 +8,13 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
+  // res.sendFile returns the file at the given path, matlab ki vo file open krdeti hai
   res.sendFile(__dirname + "/public/index.html");
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.post("/submit", (req, res) => {
+  console.log(req.body);
 });
 
 app.listen(port, () => {
